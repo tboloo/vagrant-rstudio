4abd63b88027246d2d52a4d784ae15de0b5aa9c0
Bolesław Tekielski <btekielski@gmail.com>
2015-01-06 12:45:26 +0100

    Bumped version

--- chef/cookbooks/R/metadata.rb
+++ chef/cookbooks/R/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'bolek@vault13.pl'
 license          'All rights reserved'
 description      'Provides R statistical package and RStudio server resources'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.1'
+version          '0.4.2'
 
 depends 'apt'
 depends 'git'
